3a232cfd9cb55c87e5e7572c00de4de563b9c12b
Yamandú Germano Cavalcanti <63875673+YamanduGermano@users.noreply.github.com>
2021-03-18 23:46:34 -0300

    Settings, help, about and change texts on rpc pages done.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const fetch = require('node-fetch')
 const input = require('input')
 var cfg = require('./config.json')
+const { hrtime } = require('process')
 
 
 //Command Line Interface Functions
@@ -62,6 +63,8 @@ async function login(){
     }
 }
 
+
+// PAGES -------------
 function run(){
     const client = new rpc.Client({ transport: 'ipc' })
     
@@ -78,32 +81,199 @@ function run(){
                     small_image : cfg.smimage,
                     small_text: cfg.smtext
                 },
-                buttons : cfg.links
+                buttons : cfg.bttns
             }
         })
     })
 
     console.clear()
 
-    console.log("Running Rich Presence!\n\nType CTRL+C to exit")
+    console.log("Running Rich Presence!\n\nType anything to return or press CTRL+C to exit")
+
+    breakrpc()
+
     client.login({ clientId : cfg.cid}).catch(console.error)
 }
 
+async function breakrpc(){
+    selection = await input.text("> ").catch(console.error)
+    client.destroy()
+    menu()
+}
+
+async function settings(){
+    console.clear()
+    let selection = await input.select(" \nSETTINGS\n \n \nChoose one of the options below:",[{name:'CHANGE TEXTS', value: 1}, {name:'CHANGE IMAGES', value: 2}, {name:'ADD BUTTONS', value: 3}, {name:'RESET SYSTEM', value: 4}, {name:'RELOAD ASSETS LIST', value: 5}, { name: "", disabled: true },{name:'RETURN', value: 6}])
+    switch (selection) {
+        case 1:
+            texts()
+            break
+        case 2:
+            // images()
+            break
+        case 3:
+            // buttons()
+            break
+        case 4:
+            // reset()
+            break
+        case 5:
+            // reload()
+            break
+        default:
+            fs.writeFileSync('./config.json', JSON.stringify(cfg,null,3), (err) => {if (err) console.log('Error writing file:', err)})
+            console.log(cfg)
+            menu()
+            break
+    }
+}
+
+async function texts(){
+    console.clear()
+    let selection = await input.select(" \nTEXTS\n \n \nChoose one of the options below:",[{name:`STATE: ${cfg.state}`, value: 1}, {name:`DETAILS: ${cfg.details}`, value: 2}, {name:`LARGE IMAGE TEXT: ${cfg.lgtext}`, value: 3}, {name:`SMALL IMAGE TEXT: ${cfg.smtext}`, value: 4}, { name: "", disabled: true },{name:'RETURN', value: 5}])
+    switch (selection) {
+        case 1:
+            selection = await input.text(`To return press enter\n \nOLD State: ${cfg.state}\n \nNEW State: `).catch(console.error)
+            if (selection){
+                cfg.state = selection
+                texts()
+                break
+            }
+        case 2:
+            selection = await input.text(`To return press enter\n \nOLD Detail: ${cfg.details}\n \nNEW Detail: `).catch(console.error)
+            if (selection){
+                cfg.details = selection
+                texts()
+                break
+            }
+        case 3:
+            selection = await input.text(`To return press enter\n \nOLD Large Image Text: ${cfg.lgtext}\n \nNEW Large Image Text: `).catch(console.error)
+            if (selection){
+                cfg.lgtext = selection
+                texts()
+                break
+            }
+        case 4:
+            selection = await input.text(`To return press enter\n \nOLD Small Image Text: ${cfg.smtext}\n \nNEW Small Image Text: `).catch(console.error)
+            if (selection){
+                cfg.smtext = selection
+                texts()
+                break
+            }
+        default:
+            settings()
+            break
+    }
+}
+
+async function help(){
+    console.clear()
+    console.log(`
+    So, You are having problems with using PRORPC
+    No problem, I will help you!
+
+
+    ----------| RPC Basic Setup |----------
+
+    First of all, you have to create an application on Discord Developers Page
+    Link: https://discord.com/developers
+
+    You will get its Client ID
+
+    Its name will be displayed as the game name, so in your account it will appear as
+    (If your application name is "Sua mãe")
+    "Playing Sua mãe"
+
+    ----------| Advanced Settings |----------
+
+    Ok, now your RPC is basically working.
+    Lets customize it
+
+    To add images on your RPC you have to go on Assets tab
+
+    (If you are logged in, I can get the correct Link but if you don't, you have to read the block above)
+    Link:https://discord.com/developers/applications/${cfg.cid}/rich-presence/assets
+
+    Upload your images and set their names
+
+    Assets names are going to be fetched when you do the login here
+    If you notice that some of them are missing, go on the settings and refresh the list
+
+    Now, go to the settings page and choose what you want to display in your Rich Presence.
+
+    To RETURN to MAIN MENU type anything below
+    `)
+
+    selection = await input.text("> ").catch(console.error)
+    menu()
+}
+
+async function about(){
+    console.clear()
+    console.log(`
+    ProRPC! Created by Yamandú Germano, an brazilian student and creator of Codae Channel (AD time lol)
+    Se você deseja saber mais sobre programação e como isso pode mudar seu futuro, no canal Codae você pode aprender desde o básico até o avançado de uma forma simples e acessível!    
+    
+    Now I will paste some links and Licensing things. So, if you want to back to menu, just type anything
+
+    
+    ----------| links |----------
+    My Youtube channel Codae: https://www.youtube.com/channel/UCGho6jJaDh51TqUckTqcyKg
+    Github Repository: https://github.com/YamanduGermano/ProRPC
+    -----------------------------
+    
+
+    MIT License
+
+    Copyright (c) 2021 Yamandú Germano Cavalcanti
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+    To RETURN to MAIN MENU type anything below
+    `)
+
+    selection = await input.text("> ").catch(console.error)
+    menu()
+}
 
 //Main Menu
 async function menu(){
     // If ID is registered on system
-    if (cfg.cid != ""){
-        console.clear()
-        let selection = await input.select("-----------------------\n \nWelcome to ProRPC\n \n \nChoose one of the options below:",[{name:'Run RPC', value: '1'},{name:'Options', value: '2'}, {name:'Reset system', value: '3'},{name:'Reload assets list', value: '4'}, {name: 'Exit', value: 5}])
-        function validate (value) {
-            if (value == 1){return run()}
-            else if (value == 2){return options()}
-            else if (value == 3){return reset()}
-            else if (value == 4){return reload()}
-            else{process.exit()}
+    if (cfg.cid){
+        // console.clear()
+        let selection = await input.select(" \nWelcome to ProRPC\n \n \nChoose one of the options below:",[{name:'RUN RPC', value: 1},{name:'SETTINGS', value: 2}, {name: 'ABOUT', value: 3}, {name: 'HELP', value: 4}, { name: "", disabled: true }, {name: 'EXIT', value: 5}])
+        switch (selection) {
+            case 1:
+                run()
+                break
+            case 2:
+                settings()
+                break
+            case 3:
+                about()
+                break
+            case 4:
+                help()
+                break
+            default:
+                process.exit()
         }
-        validate(selection)
     }
 
     //If user dont provide any ID
